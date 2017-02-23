d0956b7381eab5ae98619ab7ecdbb3a489940420
Jonas Almeida <jalmeida@mathbiol.org>
2017-02-23 17:14:04 -0500

    listing routes

--- root.js
+++ root.js
@@ -1,3 +1,6 @@
 module.exports = function(req,res){
-    res.end('Hello route from root :-),\nThis service is being automatically deployed from https://github.com/SBU-BMI/helloRoute')
+    res.send('Hello route from root :-),')
+    res.send('\nThis service is being automatically deployed from https://github.com/SBU-BMI/helloRoute.')
+    res.send('\nThe routes are defined at https://github.com/SBU-BMI/helloRoute/blob/master/routes.js, all others fall to the root automatically,')
+    res.end('\ni.e. try https://helloroute.herokuapp.com/jonas, https://helloroute.herokuapp.com/wade, https://helloroute.herokuapp.com/paul, etc')
 }
