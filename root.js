ffd9db4676d550973402829c17293216ebb42bc5
Jonas Almeida <jalmeida@mathbiol.org>
2017-02-23 17:06:56 -0500

    new line

--- root.js
+++ root.js
@@ -1,3 +1,3 @@
 module.exports = function(req,res){
-    res.end('hello route from root :-),\n this service is being automatically deployed from https://github.com/SBU-BMI/helloRoute')
+    res.end('Hello route from root :-),\nThis service is being automatically deployed from https://github.com/SBU-BMI/helloRoute')
 }
