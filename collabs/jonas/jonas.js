6e97d30d992f0541fafabb64fc346e623ed91eb4
Jonas Almeida <jalmeida@mathbiol.org>
2017-02-24 12:59:14 -0500

    Hi Mary

--- collabs/jonas/jonas.js
+++ collabs/jonas/jonas.js
@@ -1,3 +1,3 @@
 module.exports = function(req,res){
-    res.end('hello from "'+req.originalUrl+'" at '+Date()+', edited first via Fork, then from branch (Hi Joe)')
+    res.end('hello from "'+req.originalUrl+'" at '+Date()+', edited first via Fork, then from branch (Hi Mary)')
 }
